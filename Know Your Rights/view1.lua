0671cc3238092340104258b1599d9d3ec63b2332
Dwoodtho <Dwoodtho@our.ecu.edu.au>
2018-09-17 22:07:56 +0800

    PP Session #1 Driver: Daniel Navigator: Matt added scrollview

--- Know Your Rights/view1.lua
+++ Know Your Rights/view1.lua
@@ -6,6 +6,53 @@
 
 local composer = require( "composer" )
 local scene = composer.newScene()
+local widget = require( "widget" )
+  
+-- ScrollView listener
+local function scrollListener( event )
+ 
+    local phase = event.phase
+    if ( phase == "began" ) then print( "Scroll view was touched" )
+    elseif ( phase == "moved" ) then print( "Scroll view was moved" )
+    elseif ( phase == "ended" ) then print( "Scroll view was released" )
+    end
+ 
+    -- In the event a scroll limit is reached...
+    if ( event.limitReached ) then
+        if ( event.direction == "up" ) then print( "Reached bottom limit" )
+        elseif ( event.direction == "down" ) then print( "Reached top limit" )
+        elseif ( event.direction == "left" ) then print( "Reached right limit" )
+        elseif ( event.direction == "right" ) then print( "Reached left limit" )
+        end
+    end
+ 
+    return true
+end
+ 
+-- Create the widget
+local scrollView = widget.newScrollView(
+    {
+        top = 80,
+        left = 10,
+        width = display.contentWidth - 30,
+        height = display.contentHeight - 138,
+        scrollWidth = 600,
+        scrollHeight = 800,
+        listener = scrollListener
+		
+    }
+)
+ 
+-- Create a image and insert it into the scroll view
+--local background = display.newImageRect( "button2.png", 100, 100 )
+
+local textTest = "Overview Stop and search is a procedure used by police forces to reduce crime rates, prevent potential for crime and to protect the community. The police may seek to search anyone who is suspected of or known to possess illegal items, for example, weapons and illicit drugs. These are often performed in crime hot spot areas where suspicious activity is present."
+
+local myText = display.newText( textTest, display.contentCenterX, 100, display.contentWidth-30, 0, native.systemFont, 16 )
+myText:setFillColor( 0, 0, 0 )
+scrollView:insert( myText )	
+scrollView._view._isHorizontalScrollingDisabled = true
+
 
 function scene:create( event )
 	local sceneGroup = self.view
@@ -16,11 +63,14 @@ function scene:create( event )
 	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
 	
 	-- create a white background to fill screen
+	
+	
+	
 	local background = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
 	background:setFillColor( 1 )	-- white
 	
 	-- create some text
-	local title = display.newText( "First View", display.contentCenterX, 125, native.systemFont, 32 )
+	local title = display.newText( "Stop & Search", display.contentCenterX, 20, native.systemFont, 32 )
 	title:setFillColor( 0 )	-- black
 	
 	local newTextParams = { text = "Loaded by the first tab's\n\"onPress\" listener\nspecified in the 'tabButtons' table", 
